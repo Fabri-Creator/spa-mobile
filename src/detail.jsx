b2e51a3945b8a66e73197df60d4c4210d85d30a7
Fabricio Lancieri <fabricio.lancieri@travelport.com>
2022-11-16 17:25:15 +0100

    raw components and routing

---
+++ src/detail.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Detail() {
+    return (
+      <div className="App">
+        <Link to="/" >Product page</Link>
+        Detail
+      </div>
+    )
+  }
+  
+  export default Detail
---
+++ src/product.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Product() {
+    return (
+      <div className="App">
+        <Link to="/Detail" >Detail page</Link>
+        Product
+      </div>
+    )
+  }
+  
+  export default Product
+  
