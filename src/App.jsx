7c26bf24e19934bc6dbbe47a9c5611c8c3a2b358
Fabricio Lancieri <fabricio.lancieri@travelport.com>
2022-11-17 18:36:35 +0100

    products api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import Product from "./product";
-import Detail from "./Detail";
+import Product from "./vistas/Product";
+import Detail from "./vistas/Detail";
+ import useFetch from "./assets/useFetch";
 
-// import { createRoot } from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState('')
+
+  useEffect(() => {
+    useFetch('https://front-test-api.herokuapp.com/api/product').then(setItems)
+  }, []);
+
+  //   useEffect(() => {
+  //   console.log('PRODUCTS FILTRADOS', items)
+  // }, [items]);
+
+const handleItems = (filtrado) => {
+  
+  (!filtrado) && useFetch('https://front-test-api.herokuapp.com/api/product').then(setItems)
+
+  const filteredItems = items.filter(movile => movile.brand.includes(filtrado) || movile.model.includes(filtrado)  ) 
+  
+  console.log('PRODUCTS FILTRADOS', filteredItems);
+}
+
   return (
-    <div className="App">
+    <div className="app">
       <Routes>
-        <Route path="/" element={<Product />} />
+        <Route path="/" element={<Product items={items} handleItems={handleItems} />} />
         <Route path="/Detail" element={<Detail />} />
       </Routes>
     </div>
